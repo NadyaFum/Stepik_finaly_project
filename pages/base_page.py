482405dcfa03f775edc756a11c5d8a98e02039b4
NadyaFum <nadyafum@gmail.com>
2022-11-02 10:25:21 +0300

    added exception handling NoSuchElementException

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
-
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
@@ -8,6 +11,18 @@ class BasePage():
 
 	def open(self):
 		self.browser.get(self.url)
+
+	def __init__(self, browser, url, timeout=10):
+		self.browser = browser
+		self.url = url
+		self.browser.implicitly_wait(timeout)
+
+	def is_element_present(self, how, what):
+		try:
+			self.browser.find_element(how, what)
+		except (NoSuchElementException):
+			return False
+		return True
 
 
 
